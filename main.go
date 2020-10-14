77cbdc2ae328a96713ff739d6500752d526dccf0
deemak <deema_k@mail.ru>
2020-10-14 18:41:12 +0300

    read and write

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func check(e error) {
@@ -12,10 +13,32 @@ func check(e error) {
 	}
 }
 
-func main() {
-	dat, err := ioutil.ReadFile("data.json")
+func append() {
+	errRemove := os.Remove("data.ln")
+	check(errRemove)
+	f, err := os.OpenFile("data.ln", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	check(err)
-	var types []string
-	json.Unmarshal(dat, &types)
+	defer f.Close()
+	f.WriteString("[")
+	for i := 0; i < 1000; i++ {
+		if i == 999 {
+			if _, err = f.WriteString(fmt.Sprintf("{\"id\":%d, \"descript\":\"Test-%d\"}", i+1, i+1)); err != nil {
+				panic(err)
+			}
+			continue
+		}
+		if _, err = f.WriteString(fmt.Sprintf("{\"id\":%d, \"descript\":\"Test-%d\"},", i+1, i+1)); err != nil {
+			panic(err)
+		}
+	}
+	f.WriteString("]")
+}
+func main() {
+	append()
+	dat, err := ioutil.ReadFile("data.ln")
+	check(err)
+	var types []interface{}
+	errUnmarshal := json.Unmarshal(dat, &types)
+	check(errUnmarshal)
 	fmt.Printf("%+v", types)
 }
